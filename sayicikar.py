bdc5b97b512c396daf5486b056c5524e87b6354f
sinemyvs <80308516+sinemyvs@users.noreply.github.com>
2021-05-07 19:16:42 +0300

    Add files via upload

---
+++ sayicikar.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def TemizVeri(): 
+    a = "1234567890"
+    ilk_string ="Ah5me4t" 
+    for i in a:
+        ilk_string = ilk_string.replace(i, "")
+    ikinci_string = "M9eHm4eT"
+    for i in a:
+        ikinci_string = ikinci_string.replace(i, "")
+    ucuncu_string ="Ha3K5a1n"
+    for i in a:
+        ucuncu_string = ucuncu_string.replace(i, "")
+    Birlesmis_deger="-".join([ilk_string,ikinci_string,ucuncu_string]) 
+    print(Birlesmis_deger)
+    return Birlesmis_deger
+TemizVeri()
+
